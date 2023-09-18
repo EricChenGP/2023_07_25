a025278151ab93c6a2a9597ed5b7cdc272284df2
EricChenGP <140509752+EricChenGP@users.noreply.github.com>
2023-09-18 05:04:26 +0000

    modify

---
+++ hw1.java
@@ -0,0 +1,9 @@
+public class hw1
+{
+    public static void main(String[] args) // required
+    {
+        System.out.println("\t歡迎光臨--成大電雞\t");
+        System.out.println("\t只有你想不到，沒有我麼做不到\t");
+        System.out.println("Hello reader."); // a statement
+    }
+}
